7323a9ef6289ba68f9ea272f60a978f8824645b9
EddisonChen <eddisonchen1@gmail.com>
2023-06-01 00:03:24 -0400

    calorie and bmi calculation completed

--- function.js
+++ function.js
@@ -5,9 +5,15 @@ const weightInput = document.getElementById("weight-input");
 const submitButton = document.getElementById("submit-button");
 const result = document.getElementById("result-message");
 const male = document.getElementById("male");
-const female = document.getElementById("female")
+const female = document.getElementById("female");
+const sexContainer = document.getElementById("sex");
+const setAge = document.getElementById("age");
+const setActivityLevel = document.getElementById("activity-level");
+const setBodyFatPercentage = document.getElementById("body-fat-percentage");
 
 let unitType = null;
+let heightUnit = null;
+let weightUnit = null;
 
 imperial.addEventListener('click', () => {
     heightInput.innerHTML = (
@@ -48,7 +54,10 @@ imperial.addEventListener('click', () => {
     weightInput.innerHTML = (
         `Weight: <input type="number" placeholder="Weight in LBs" id="weight" pattern="[1-9]|[1-9][0-9]|[1-9][0-9][0-9]">`
     )
-    unitType = "imperial";
+    unitType = "imperial"
+    heightUnit = "inches"
+    weightUnit = "pounds"
+    console.log(unitType)
 })
 
 metric.addEventListener('click', () => {
@@ -59,11 +68,27 @@ metric.addEventListener('click', () => {
         `Weight: <input type="number" placeholder="Weight in KGs" id="weight" pattern="[1-9]|[1-9][0-9]|[1-9][0-9][0-9]">`
     )
     unitType = "metric";
+    heightUnit = "centimeters"
+    weightUnit = "kilograms"
+    console.log(unitType)
 })
 
 let height = null;
 let weight = null;
 let sex = null;
+let age = null;
+let activityLevel = "sedentary";
+let bodyFatPercentage = null;
+
+setActivityLevel.addEventListener("change", () => {
+    activityLevel = setActivityLevel.value;
+    console.log(activityLevel)
+})
+
+setBodyFatPercentage.addEventListener('change', () => {
+    bodyFatPercentage = setBodyFatPercentage.value
+    console.log(bodyFatPercentage)
+})
 
 const setHeight = () => {
     const heightValue = document.getElementById("height");
@@ -77,27 +102,225 @@ const setWeight = () => {
     console.log(weight)
 }
 
-male.addEventListener('click', () => {
-    sex = "male";
+setAge.addEventListener('change', () => {
+    age = setAge.value
+    console.log(age)
 })
 
-female.addEventListener('click', () => {
-    sex = "female";
+sexContainer.addEventListener('click', () => {
+    if (male.checked) {
+        sex = male.value;
+    } else if (female.checked) {
+        sex = female.value
+    }
+    console.log(sex)
 })
+
+let bmrCalories = null;
+let tdeeCalories = null;
+let bmi = null;
+let bmiClass = null;
+let calorieActivityRange = {};
+let lbm = null;
+
+const mifflinStJeor = (weight, height, age, sex) => {
+    bmrCalories = ((10*weight) + (6.25*height) - (5*age) + sex)
+}
+
+const beor = (weight, height) => {
+    if (sex=="male") {
+        lbm = ((.407*weight) + (.267*height) - 19.2)
+    } else if (sex =="female"){
+        lbm = ((.252*weight) + (.473*height) - 48.3)
+    }
+}
+
+const katchMcArdle = () => {
+    bmrCalories = (370 + (21.6*lbm))
+}
+
+const calorieCalculation = () => {
+    const convertedHeight = parseInt(height)*2.54
+    const convertedWeight = parseInt(weight)/2.205
+    let sexNumericValue = null;
+    if (sex == "male") {
+        sexNumericValue = 5
+    } else {
+        sexNumericValue = - 161
+    }
+    if (bodyFatPercentage == null || bodyFatPercentage == "") {
+        if (unitType == "imperial") {
+            mifflinStJeor(convertedWeight, convertedHeight, age, sexNumericValue)
+            console.log("using imperial", height, weight, sexNumericValue, age)
+        } else {
+            mifflinStJeor(weight, height, age, sexNumericValue)
+            console.log("using metric", height, weight, sexNumericValue, age)
+        }
+    } 
+    else {
+        if (unitType == "imperial") {
+            beor(convertedWeight, convertedHeight)
+            katchMcArdle()
+            console.log("using imperial, katch", height, weight, sexNumericValue, age)
+        } else {
+            beor(weight, height)
+            katchMcArdle()
+            console.log("using metric, katch", height, weight, sexNumericValue, age)
+        }
+    }
+    console.log(bmrCalories)
+    calorieActivityRange = {
+        "bmr": bmrCalories.toFixed(),
+        "sedentary": (bmrCalories * 1.2).toFixed(),
+        "lightExercise": (bmrCalories * 1.275).toFixed(),
+        "moderateExercise": (bmrCalories * 1.55).toFixed(),
+        "heavyExercise": (bmrCalories * 1.725).toFixed(),
+        "athlete": (bmrCalories * 1.9).toFixed()
+    }
+}
+
+const bmiCalculation = () => {
+    if (unitType == "imperial") {
+        bmi = (703 * (weight/Math.pow(height, 2))).toFixed(1)
+    } else {
+        bmi = (weight/Math.pow((height/100), 2)).toFixed(1)
+    }
+    if (bmi < 18.5) {
+        bmiClass = "underweight"
+    } else if (bmi >= 18.5 && bmi < 25) {
+        bmiClass = "normal weight"
+    } else if (bmi >= 25 && bmi < 30) {
+        bmiClass = "overweight"
+    } else if (bmi >= 30) {
+        bmiClass = "obese"
+    }
+}
+
+// const resultMessage = (
+//     <div>
+//         <p>You are a ${age} year old ${sex} that is ${height} ${heightUnit} tall and weighs ${weight} ${weightUnit}.</p>
+//         <h2>Your maintenance calories: ${calorieActivityRange[activityLevel]} per day</h2>
+//         <table>
+//             <tr>
+//                 <td>Basal Metabolic Rate</td>
+//                 <td>${calorieActivityRange["bmr"]}</td>
+//             </tr>
+//             <tr>
+//                 <td>Sedentary</td>
+//                 <td>${calorieActivityRange["sedentary"]}</td>
+//             </tr>
+//             <tr>
+//                 <td>Light Exercise</td>
+//                 <td>${calorieActivityRange["lightExercise"]}</td>
+//             </tr>
+//             <tr>
+//                 <td>Moderate Exercise</td>
+//                 <td>${calorieActivityRange["moderateExercise"]}</td>
+//             </tr>
+//             <tr>
+//                 <td>Heavy Exercise</td>
+//                 <td>${calorieActivityRange["bmr"]}</td>
+//             </tr>
+//             <tr>
+//                 <td>Athlete</td>
+//                 <td>${calorieActivityRange["athlete"]}</td>
+//             </tr>
+//         </table>
+//         <div>
+//             <h3>BMI Score: ${bmi}</h3>
+//             <p>You are classified as ${bmiClass}</p>
+//             <table>
+//                 <tr>
+//                     <td>18.5 or Under</td>
+//                     <td>Underweight</td>
+//                 </tr>
+//                 <tr>
+//                     <td>18.5 - 24.99</td>
+//                     <td>Normal Weight</td>
+//                 </tr>
+//                 <tr>
+//                     <td>25 - 29.99</td>
+//                     <td>Overweight</td>
+//                 </tr>
+//                 <tr>
+//                     <td>30 or Over</td>
+//                     <td>Obese</td>
+//                 </tr>
+//             </table>
+//         </div>
+//     </div>
+// )
 
 submitButton.addEventListener('click', () => {
     setHeight()
     setWeight()
-    if (height && weight &&sex) {
+    calorieCalculation()
+    bmiCalculation()
+    if (height && weight && sex) {
         result.innerHTML = (
-            `<p>good job</p>`
-        )
+            `<div>
+            <p>You are a ${age} year old ${sex} that is ${height} ${heightUnit} tall and weighs ${weight} ${weightUnit}.</p>
+            <h2>Your maintenance calories: ${calorieActivityRange[activityLevel]} per day</h2>
+            <table>
+                <tr>
+                    <td>Basal Metabolic Rate</td>
+                    <td>${calorieActivityRange["bmr"]} calories per day</td>
+                </tr>
+                <tr>
+                    <td>Sedentary</td>
+                    <td>${calorieActivityRange["sedentary"]} calories per day</td>
+                </tr>
+                <tr>
+                    <td>Light Exercise</td>
+                    <td>${calorieActivityRange["lightExercise"]} calories per day</td>
+                </tr>
+                <tr>
+                    <td>Moderate Exercise</td>
+                    <td>${calorieActivityRange["moderateExercise"]} calories per day</td>
+                </tr>
+                <tr>
+                    <td>Heavy Exercise</td>
+                    <td>${calorieActivityRange["heavyExercise"]} calories per day</td>
+                </tr>
+                <tr>
+                    <td>Athlete</td>
+                    <td>${calorieActivityRange["athlete"]} calories per day</td>
+                </tr>
+            </table>
+            <div>
+                <h3>BMI Score: ${bmi}</h3>
+                <p>You are classified as ${bmiClass}</p>
+                <table>
+                    <tr>
+                        <td>18.5 or Under</td>
+                        <td>Underweight</td>
+                    </tr>
+                    <tr>
+                        <td>18.5 - 24.99</td>
+                        <td>Normal Weight</td>
+                    </tr>
+                    <tr>
+                        <td>25 - 29.99</td>
+                        <td>Overweight</td>
+                    </tr>
+                    <tr>
+                        <td>30 or Over</td>
+                        <td>Obese</td>
+                    </tr>
+                </table>
+            </div>
+        </div>`
+        );
     } else {
         result.innerHTML = (
             `<p>bad dog</p>`
         )
     }
 })
+
+
+
+
 
 
 
